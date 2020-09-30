71024bec19f13fe1caf5f3ead4f2b152ec2e1795
Mark Howe <mvahowe@gmail.com>
2020-09-30 13:01:52 +0200

    Port REMs test from anastamosis

---
+++ test/code/graft_rems.js
@@ -0,0 +1,25 @@
+const test = require('tape');
+
+const {pkWithDoc} = require('../lib/load');
+
+const testGroup = "Graft Headings";
+
+const pk = pkWithDoc("../test_data/usfm/rems.usfm", "fra", "hello")[0];
+
+test(
+    `REMs (${testGroup})`,
+    async function (t) {
+        t.plan(4);
+        const query = '{ documents { sequences { id type blocks { bg { type, sequenceId } text } } mainSequence { id } } }';
+        const result = await pk.gqlQuery(query);
+        t.ok("data" in result);
+        const sequences = {};
+        for (const seq of result.data.documents[0].sequences) {
+            sequences[seq.id] = seq;
+        }
+        const mainSequence = sequences[result.data.documents[0].mainSequence.id];
+        t.equal(mainSequence.blocks[0].bg.length, 1);
+        t.equal(mainSequence.blocks[0].bg[0].type, "remark");
+        t.equal(sequences[mainSequence.blocks[0].bg[0].sequenceId].blocks[0].text, "Need to finish this sentence");
+    }
+);
