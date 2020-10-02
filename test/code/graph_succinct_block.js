6b61c33cfa0aa5cb9b6c5e83f77b1039237d7c01
Mark Howe <mvahowe@gmail.com>
2020-10-02 12:12:57 +0200

    Succinct test with shorter USX document that populates all succinct fields

--- test/code/graph_succinct_block.js
+++ test/code/graph_succinct_block.js
@@ -4,25 +4,35 @@ const {pkWithDoc} = require('../lib/load');
 
 const testGroup = "Graph Succinct Block";
 
-const pk = pkWithDoc("../test_data/usx/web_rut.usx", "eng", "ust")[0];
+const pk = pkWithDoc("../test_data/usx/web_rut_1.usx", "eng", "ust")[0];
 
 test(
     `Byte Length (${testGroup})`,
     async function (t) {
-        const lengths = {
-            cByteLength: 202,
-            bgByteLength: 0,
-            osByteLength: 9,
-            isByteLength: 6
-        };
-        t.plan(2 + Object.keys(lengths).length);
+        const lengths = [
+            {
+                cByteLength: 1734,
+                bgByteLength: 9,
+                osByteLength: 0,
+                isByteLength: 57
+            },
+            {
+                cByteLength: 198,
+                bgByteLength: 0,
+                osByteLength: 9,
+                isByteLength: 6
+            }
+        ];
+        t.plan(2 + (lengths.length * Object.keys(lengths[0]).length));
         const query = '{ documents { mainSequence { succinctBlocks { cByteLength bgByteLength osByteLength isByteLength } } } }';
         const result = await pk.gqlQuery(query);
         t.ok("data" in result);
-        const block = result.data.documents[0].mainSequence.succinctBlocks[1];
         t.ok("succinctBlocks" in result.data.documents[0].mainSequence);
-        for (const [field, value] of Object.entries(lengths)) {
-            t.equal(block[field], value);
+        for (const blockNo of [0, 1]) {
+            const block = result.data.documents[0].mainSequence.succinctBlocks[blockNo];
+            for (const [field, value] of Object.entries(lengths[blockNo])) {
+                t.equal(block[field], value);
+            }
         }
     }
 );
