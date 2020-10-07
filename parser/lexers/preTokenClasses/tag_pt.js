af3423da3d31739c0a864f5aabeded1ee0d96350
Mark Howe <mvahowe@gmail.com>
2020-10-07 09:37:38 +0200

    Lexing regex and preToken support backslash-plus notation re #2

--- parser/lexers/preTokenClasses/tag_pt.js
+++ parser/lexers/preTokenClasses/tag_pt.js
@@ -6,6 +6,11 @@ class TagPT extends PreToken {
         // console.log(matchedBits);
         super(subclass);
         this.tagName = matchedBits[2];
+        this.isNested = false;
+        if (this.tagName.startsWith("+")) {
+            this.isNested = true;
+            this.tagName = this.tagName.substring(1);
+        }
         this.tagLevel = matchedBits[3] !== "" ? parseInt(matchedBits[3]) : 1;
         this.fullTagName = `${this.tagName}${matchedBits[3] === "1" ? "" : matchedBits[3]}`;
         this.printValue = subclass === "startTag" ? `\\${this.fullTagName} ` : `\\${this.fullTagName}*`;
