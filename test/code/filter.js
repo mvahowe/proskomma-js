917d2b511f07da21a347a0dc962f562cf3fe3249
Mark Howe <mvahowe@gmail.com>
2020-10-12 09:53:09 +0200

    Test for inline grafts closes #29

--- test/code/filter.js
+++ test/code/filter.js
@@ -6,11 +6,15 @@ const testGroup = "Filter on Parse";
 
 const queryToBlock = async (t, filter) => {
     const pk = pkWithDoc("../test_data/usfm/filter.usfm", "fra", "hello", filter)[0];
-    const query =
-        '{ documents { mainSequence { blocks { bg { type } bs { label } scopeLabels } } } }';
+    const itemFragment = '{ ... on Token { subType chars dump } ... on Scope { subType label dump } ... on Graft { type sequenceId dump } }';
+    const query = `{ documents { mainSequence { blocks { bg { type } bs { label } scopeLabels c ${itemFragment} } } } }`;
     const result = await pk.gqlQuery(query);
     t.ok("data" in result);
     return result.data.documents[0].mainSequence.blocks[0];
+}
+
+const inlineGrafts = block => {
+    return block.c.filter(i => i.type);
 }
 
 test(
@@ -25,7 +29,7 @@ test(
                 "span/it",
                 "span/bd"
             ];
-            t.plan(5 + scopeLabels.length);
+            t.plan(6 + scopeLabels.length);
             const block = await queryToBlock(t,{});
             t.equal(block.bg.length, 2);
             t.equal(block.bg[0].type, "title");
@@ -34,6 +38,8 @@ test(
             for (const scopeLabel of scopeLabels) {
                 t.ok(block.scopeLabels.includes(scopeLabel));
             }
+            const inlines = inlineGrafts(block);
+            t.equal(inlines.length, 1);
         } catch (err) {
             console.log(err)
         }
@@ -44,10 +50,12 @@ test(
     `Exclude All Scopes (${testGroup})`,
     async function (t) {
         try {
-            t.plan(3);
+            t.plan(4);
             const block = await queryToBlock(t,{includeScopes: []});
             t.equal(block.bg.length, 2);
             t.equal(block.scopeLabels.length, 0);
+            const inlines = inlineGrafts(block);
+            t.equal(inlines.length, 1);
         } catch (err) {
             console.log(err)
         }
@@ -58,10 +66,12 @@ test(
     `Exclude All Grafts (${testGroup})`,
     async function (t) {
         try {
-            t.plan(3);
+            t.plan(4);
             const block = await queryToBlock(t,{includeGrafts: []});
             t.equal(block.bg.length, 0);
             t.equal(block.scopeLabels.length, 6);
+            const inlines = inlineGrafts(block);
+            t.equal(inlines.length, 0);
         } catch (err) {
             console.log(err)
         }
@@ -72,10 +82,12 @@ test(
     `Exclude Everything (${testGroup})`,
     async function (t) {
         try {
-            t.plan(3);
+            t.plan(4);
             const block = await queryToBlock(t,{includeGrafts: [], includeScopes: []});
             t.equal(block.bg.length, 0);
             t.equal(block.scopeLabels.length, 0);
+            const inlines = inlineGrafts(block);
+            t.equal(inlines.length, 0);
         } catch (err) {
             console.log(err)
         }
@@ -86,10 +98,12 @@ test(
     `Exclude Nothing (${testGroup})`,
     async function (t) {
         try {
-            t.plan(3);
+            t.plan(4);
             const block = await queryToBlock(t,{excludeGrafts: [], excludeScopes: []});
             t.equal(block.bg.length, 2);
             t.equal(block.scopeLabels.length, 6);
+            const inlines = inlineGrafts(block);
+            t.equal(inlines.length, 1);
         } catch (err) {
             console.log(err)
         }
@@ -166,3 +180,32 @@ test(
     }
 );
 
+test(
+    `Include Inline (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(3);
+            const block = await queryToBlock(t,{includeGrafts: ["footnote"]});
+            t.equal(block.bg.length, 0);
+            const inlines = inlineGrafts(block);
+            t.equal(inlines.length, 1);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+);
+
+test(
+    `Exclude Inline (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(3);
+            const block = await queryToBlock(t,{excludeGrafts: ["footnote"]});
+            t.equal(block.bg.length, 2);
+            const inlines = inlineGrafts(block);
+            t.equal(inlines.length, 0);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+);
