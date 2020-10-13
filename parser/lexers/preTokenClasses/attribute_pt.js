7c743b11d7af3ec1c6c3c62bb09f3f5806e3a444
Mark Howe <mvahowe@gmail.com>
2020-10-13 12:01:19 +0200

    Multiple replace of literal slashes

--- parser/lexers/preTokenClasses/attribute_pt.js
+++ parser/lexers/preTokenClasses/attribute_pt.js
@@ -5,7 +5,7 @@ class AttributePT extends PreToken {
     constructor(subclass, matchedBits) {
         super(subclass);
         this.key = matchedBits[2];
-        this.valueString = matchedBits[3].trim().replace("/","÷");
+        this.valueString = matchedBits[3].trim().replace(/\//g,"÷");
         this.values = this.valueString.split(",").map(vb => vb.trim());
         this.printValue = `| ${this.key}="${this.valueString}"`;
     }
