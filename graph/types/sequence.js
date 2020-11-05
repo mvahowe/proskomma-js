4330453df958cf6a8f533a2d18a3b3778ca6de58
Mark Howe <mvahowe@gmail.com>
2020-11-05 12:10:24 +0100

    filter blocks by Strongs

--- graph/types/sequence.js
+++ graph/types/sequence.js
@@ -1,4 +1,5 @@
 const {GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList, GraphQLNonNull} = require('graphql');
+const xre = require('xregexp');
 
 const blockType = require('./block');
 
@@ -17,6 +18,45 @@ const htmlFoot = s => {
     return "</body>\n</html>\n";
 }
 
+const blockHasStrongs = (docSet, block, strongs, requireAll) => {
+
+    const cleanStrong= (s) => {
+        return xre.match(s, xre("[HG][0-9]+"));
+    }
+
+    if (strongs.length === 0) {
+        return true;
+    }
+    let matched = new Set([]);
+    const options = {
+        tokens: false,
+        scopes: true,
+        grafts: false,
+        requiredScopes: []
+    };
+    for (const item of docSet.unsuccinctifyPrunedItems(block, options)) {
+        const [att, attType, element, key, value] = item[1].split("/");
+        if (
+            (attType === "spanWithAtts" && element === "w" && key === "strong" && strongs.includes(cleanStrong(value))) ||
+            (attType === "milestone" && element === "zaln" && key === "x-strong" && strongs.includes(cleanStrong(value)))
+        ) {
+            matched.add(value);
+            if (!requireAll || matched.size === strongs.length) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+const checkStrongsFormat = (strongs) => {
+    for (const strong of strongs) {
+        if (!xre.match(strong, xre("^[HG][0-9]+$"))) {
+            throw new Error(`Bad Strongs format '${strong} in query'`);
+        }
+    }
+}
+
 const sequenceType = new GraphQLObjectType({
     name: "Sequence",
     fields: () => ({
@@ -29,20 +69,35 @@ const sequenceType = new GraphQLObjectType({
             type: GraphQLNonNull(GraphQLList(GraphQLNonNull(blockType))),
             args: {
                 withScopes: {type: GraphQLList(GraphQLNonNull(GraphQLString))},
-                withScriptureCV: {type: GraphQLString}
+                withScriptureCV: {type: GraphQLString},
+                withAllStrongs: {type: GraphQLList(GraphQLNonNull(GraphQLString))},
+                withAnyStrongs: {type: GraphQLList(GraphQLNonNull(GraphQLString))}
             },
             resolve: (root, args, context) => {
                 context.docSet.maybeBuildEnumIndexes();
                 if (args.withScopes && args.withScriptureCV) {
                     throw new Error("Cannot specify both withScopes and withScriptureCV");
                 }
-                if (args.withScopes) {
-                    return root.blocks.filter(b => context.docSet.allScopesInBlock(b, args.withScopes));
-                } else if (args.withScriptureCV) {
-                    return context.docSet.blocksWithScriptureCV(root.blocks, args.withScriptureCV);
-                } else {
-                    return root.blocks;
+                if (args.withAllStrongs && args.withAnyStrongs) {
+                    throw new Error("Cannot specify both withAllStrongs and withAnyStrongs");
                 }
+                let ret;
+                if (args.withScopes) {
+                    ret = root.blocks.filter(b => context.docSet.allScopesInBlock(b, args.withScopes));
+                } else if (args.withScriptureCV) {
+                    ret = context.docSet.blocksWithScriptureCV(root.blocks, args.withScriptureCV);
+                } else {
+                    ret = root.blocks;
+                }
+                if (args.withAllStrongs) {
+                    checkStrongsFormat(args.withAllStrongs);
+                    ret = ret.filter(b => blockHasStrongs(context.docSet, b, args.withAllStrongs, true));
+                }
+                if (args.withAnyStrongs) {
+                    checkStrongsFormat(args.withAnyStrongs);
+                    ret = ret.filter(b => blockHasStrongs(context.docSet, b, args.withAnyStrongs, false));
+                }
+                return ret;
             }
         }
     })
