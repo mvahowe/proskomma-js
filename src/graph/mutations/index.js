86d1c4c45deb0b12611e8382be717e70a59bd4e2
Mark Howe <mvahowe@gmail.com>
2021-01-18 17:28:14 +0100

    addSequenceTags mutation re #47

--- src/graph/mutations/index.js
+++ src/graph/mutations/index.js
@@ -1,6 +1,7 @@
 const {
   GraphQLObjectType, GraphQLString, GraphQLList, GraphQLNonNull,
 } = require('graphql');
+const { addTag } = require('proskomma-utils');
 
 const schemaMutations = new GraphQLObjectType({
   name: 'Mutation',
@@ -35,6 +36,25 @@ const schemaMutations = new GraphQLObjectType({
           document.addTag(tag);
         }
         return Array.from(document.tags);
+      },
+    },
+    addSequenceTags: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(GraphQLString))),
+      args: {
+        docSetId: { type: GraphQLNonNull(GraphQLString) },
+        documentId: { type: GraphQLNonNull(GraphQLString) },
+        sequenceId: { type: GraphQLNonNull(GraphQLString) },
+        tags: { type: GraphQLNonNull(GraphQLList(GraphQLNonNull(GraphQLString))) },
+      },
+      resolve: (root, args) => {
+        const docSet = root.docSets[args.docSetId];
+        const document = docSet.processor.documents[args.documentId];
+        const sequence = document.sequences[args.sequenceId];
+
+        for (const tag of args.tags) {
+          addTag(sequence.tags, tag);
+        }
+        return Array.from(sequence.tags);
       },
     },
   },
--- src/test/code/mutate_tags.js
+++ src/test/code/mutate_tags.js
@@ -40,11 +40,34 @@ test(
       t.equal(document.tags.length, 0);
       query = `mutation { addDocumentTags(docSetId: "${docSet.id}", documentId: "${document.id}", tags: ["foo", "baa"]) }`;
       result = await pk.gqlQuery(query);
-      console.log(result);
       t.equal(result.data.addDocumentTags.length, 2);
       query = `mutation { addDocumentTags(docSetId: "${docSet.id}", documentId: "${document.id}", tags: ["foo", "frob"]) }`;
       result = await pk.gqlQuery(query);
       t.equal(result.data.addDocumentTags.length, 3);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+);
+
+test(
+  `Sequence (${testGroup})`,
+  async function (t) {
+    try {
+      t.plan(4);
+      let query = '{ docSets { id documents { id mainSequence { id tags } } } }';
+      let result = await pk.gqlQuery(query);
+      t.equal(result.errors, undefined);
+      const docSet = result.data.docSets[0];
+      const document = docSet.documents[0];
+      const sequence = document.mainSequence;
+      t.equal(sequence.tags.length, 0);
+      query = `mutation { addSequenceTags(docSetId: "${docSet.id}", documentId: "${document.id}", sequenceId: "${sequence.id}", tags: ["foo", "baa"]) }`;
+      result = await pk.gqlQuery(query);
+      t.equal(result.data.addSequenceTags.length, 2);
+      query = `mutation { addSequenceTags(docSetId: "${docSet.id}", documentId: "${document.id}", sequenceId: "${sequence.id}", tags: ["foo", "frob"]) }`;
+      result = await pk.gqlQuery(query);
+      t.equal(result.data.addSequenceTags.length, 3);
     } catch (err) {
       console.log(err);
     }
