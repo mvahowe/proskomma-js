f113e8e46b6d7b57b9350d9de4eb8bb340e257bd
Mark Howe <mvahowe@gmail.com>
2021-04-26 18:47:29 +0200

    Profiling for USFM lexer

--- src/parser/lexers/usfm/index.js
+++ src/parser/lexers/usfm/index.js
@@ -7,9 +7,17 @@ const {
 const { preTokenClassForFragment } = require('../class_for_fragment');
 
 const parseUsfm = (str, parser) => {
+  let ptTime = 0;
+  let piTime = 0;
   for (const match of xre.match(str, mainRegex, 'all')) {
-    parser.parseItem(preTokenClassForFragment(match, lexingRegexes));
+    let t = Date.now();
+    const preToken = preTokenClassForFragment(match, lexingRegexes);
+    ptTime += Date.now() - t;
+    t = Date.now();
+    parser.parseItem(preToken);
+    piTime += Date.now() - t;
   }
+  // console.log(ptTime, piTime);
 };
 
 module.exports = { parseUsfm };
