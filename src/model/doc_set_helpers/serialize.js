4027cbe85317c7c9f8496e970b4157081a2c693e
Mark Howe <mvahowe@gmail.com>
2021-06-23 16:53:12 +0200

    Complete moving of docSet functionality to helper files

---
+++ src/model/doc_set_helpers/serialize.js
@@ -0,0 +1,21 @@
+const serializeSuccinct = docSet => {
+  const ret = {
+    id: docSet.id,
+    metadata: { selectors: docSet.selectors },
+    enums: {},
+    docs: {},
+    tags: Array.from(docSet.tags),
+  };
+
+  for (const [eK, eV] of Object.entries(docSet.enums)) {
+    ret.enums[eK] = eV.base64();
+  }
+  ret.docs = {};
+
+  for (const docId of docSet.docIds) {
+    ret.docs[docId] = docSet.processor.documents[docId].serializeSuccinct();
+  }
+  return ret;
+};
+
+module.exports = { serializeSuccinct };
