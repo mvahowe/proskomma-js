d607f80506fe12dbe7fdef337b0bf4fe6789bfe1
Mark Howe <mvahowe@gmail.com>
2021-07-20 17:06:18 +0200

    cleanUSFM

---
+++ test/code/clean_usfm.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const test = require('tape');
+const fse = require('fs-extra');
+const { Proskomma } = require('../../src');
+
+const testGroup = 'cleanUSFM';
+
+test(
+  `Remove chapters (${testGroup})`,
+  async function (t) {
+    try {
+      t.plan(2);
+      const pk = new Proskomma();
+      let fp = '../test_data/usfm/frt.usfm';
+      let content = fse.readFileSync(path.resolve(__dirname, fp));
+      let cleanedLines = pk.cleanUsfm(content, { remove: ['\\c'] })
+        .split('\n')
+        .filter(l => l.length > 0);
+      t.equal(cleanedLines.length, 4);
+      t.equal(cleanedLines[1], '\\mt1 USFM');
+    } catch (err) {
+      console.log(err);
+    }
+  },
+);
+
+test(
+  `Keep chapters (${testGroup})`,
+  async function (t) {
+    try {
+      t.plan(3);
+      const pk = new Proskomma();
+      let fp = '../test_data/usfm/frt.usfm';
+      let content = fse.readFileSync(path.resolve(__dirname, fp));
+      let cleanedLines = pk.cleanUsfm(content)
+        .split('\n')
+        .filter(l => l.length > 0);
+      t.equal(cleanedLines.length, 5);
+      t.equal(cleanedLines[1], '\\mt1 USFM');
+      t.equal(cleanedLines[3], '\\c 1');
+    } catch (err) {
+      console.log(err);
+    }
+  },
+);
