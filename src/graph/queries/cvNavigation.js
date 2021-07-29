99dbad64ab80bf940dd2bcc528963f85bc09f673
Mark Howe <mvahowe@gmail.com>
2021-07-29 18:27:48 +0200

    previous/next verseRangeVerse closes #99

--- src/graph/queries/cvNavigation.js
+++ src/graph/queries/cvNavigation.js
@@ -7,6 +7,75 @@ const {
 
 const cvType = require('./cv');
 
+const nv = (root, newVerseRange) => {
+  const chapterN = parseInt(root[0]);
+  const verseN = parseInt(root[1]);
+
+  if (root[3].length <= verseN || root[3][verseN].length === 0) {
+    return null;
+  }
+  let ret = null;
+  let nc = chapterN;
+  let nv = verseN;
+  let index = root[3];
+  let startVerseRange = index[verseN][0].verses;
+  let onNextChapter = false;
+
+  while (!ret) {
+    nv += 1;
+
+    if (nv >= index.length) {
+      if (onNextChapter || !root[4]) {
+        break;
+      }
+      nv = -1;
+      nc += 1;
+      index = root[4];
+      onNextChapter = true;
+    } else if (
+      index[nv].length > 0 &&
+      (!newVerseRange || onNextChapter || index[nv][0].verses !== startVerseRange)
+    ) {
+      ret = [nc, nv];
+    }
+  }
+  return ret;
+};
+
+const pv = (root, newVerseRange) => {
+  const chapterN = parseInt(root[0]);
+  const verseN = parseInt(root[1]);
+
+  if (root[3].length <= verseN || root[3][verseN].length === 0) {
+    return null;
+  }
+  let ret = null;
+  let nc = chapterN;
+  let nv = verseN;
+  let index = root[3];
+  let startVerseRange = index[verseN][0].verses;
+  let onPreviousChapter = false;
+
+  while (!ret) {
+    nv -= 1;
+    if (nv < 0) {
+      if (onPreviousChapter || !root[2]) {
+        break;
+      }
+      nv = root[2].length;
+      nc -= 1;
+      index = root[2];
+      onPreviousChapter = true;
+    } else if (
+      index[nv].length > 0 &&
+      (!newVerseRange || onPreviousChapter || index[nv][0].verses !== startVerseRange)
+    ) {
+      ret = [nc, nv];
+    }
+  }
+  return ret;
+};
+
 const cvNavigationType = new GraphQLObjectType({
   // root is [ < 0 chapter >, < 1 verse >, < 2 previousChapterIndex >, < 3 thisChapterIndex >, < 4 nextChapterIndex > ]
   name: 'cvNavigation',
@@ -15,69 +84,22 @@ const cvNavigationType = new GraphQLObjectType({
     nextVerse: {
       type: cvType,
       description: 'The verse number for the next verse',
-      resolve: root => {
-        const chapterN = parseInt(root[0]);
-        const verseN = parseInt(root[1]);
-
-        if (root[3].length <= verseN || root[3][verseN].length === 0) {
-          return null;
-        }
-        let ret = null;
-        let nc = chapterN;
-        let nv = verseN;
-        let index = root[3];
-        let onNextChapter = false;
-
-        while (!ret) {
-          nv += 1;
-
-          if (nv >= index.length) {
-            if (onNextChapter || !root[4]) {
-              break;
-            }
-            nv = -1;
-            nc += 1;
-            index = root[4];
-            onNextChapter = true;
-          } else if (index[nv].length > 0) {
-            ret = [nc, nv];
-          }
-        }
-        return ret;
-      },
+      resolve: root => nv(root, false),
     },
     previousVerse: {
       type: cvType,
       description: 'The verse number for the previous verse',
-      resolve: root => {
-        const chapterN = parseInt(root[0]);
-        const verseN = parseInt(root[1]);
-
-        if (root[3].length <= verseN || root[3][verseN].length === 0) {
-          return null;
-        }
-        let ret = null;
-        let nc = chapterN;
-        let nv = verseN;
-        let index = root[3];
-        let onPreviousChapter = false;
-
-        while (!ret) {
-          nv -= 1;
-          if (nv < 0) {
-            if (onPreviousChapter || !root[2]) {
-              break;
-            }
-            nv = root[2].length;
-            nc -= 1;
-            index = root[2];
-            onPreviousChapter = true;
-          } else if (index[nv].length > 0) {
-            ret = [nc, nv];
-          }
-        }
-        return ret;
-      },
+      resolve: root => pv(root, false),
+    },
+    nextVerseRangeVerse: {
+      type: cvType,
+      description: 'The verse number for the next verse range',
+      resolve: root => nv(root, true),
+    },
+    previousVerseRangeVerse: {
+      type: cvType,
+      description: 'The verse number for the previous verse range',
+      resolve: root => pv(root, true),
     },
     nextChapter: {
       type: GraphQLString,
