5565022325c93cc79178866a5da6ce4dcebaf0b1
Mark Howe <mvahowe@gmail.com>
2021-09-07 12:00:27 +0200

    Table cells

---
+++ src/graph/queries/cell_type.js
@@ -0,0 +1,60 @@
+const {
+  GraphQLNonNull,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+} = require('graphql');
+const itemType = require('./item');
+
+const cellType = new GraphQLObjectType({
+  name: 'cell',
+  description: 'A table cell',
+  fields: () => ({
+    rows: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(GraphQLInt))),
+      description: 'The row numbers',
+      resolve: root => root[0],
+    },
+    columns: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(GraphQLInt))),
+      description: 'The column numbers',
+      resolve: root => root[1],
+    },
+    items: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(itemType))),
+      description: 'A list of items from the c (content) field of the cell',
+      resolve: root => root[2],
+    },
+    tokens: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(itemType))),
+      description: 'A list of tokens from the c (content) field of the cell',
+      resolve: root => root[2].filter(i => i[0] === 'token'),
+    },
+    text: {
+      type: GraphQLNonNull(GraphQLString),
+      description: 'The text of the cell as a single string',
+      args: {
+        normalizeSpace: {
+          type: GraphQLBoolean,
+          description: 'If true, converts each whitespace character to a single space',
+        },
+      },
+      resolve: (root, args) => {
+        let ret = root[2]
+          .filter(i => i[0] === 'token')
+          .map(t => t[2])
+          .join('')
+          .trim();
+
+        if (args.normalizeSpace) {
+          ret = ret.replace(/[ \t\n\r]+/g, ' ');
+        }
+        return ret;
+      },
+    },
+  }),
+});
+
+module.exports = cellType;
