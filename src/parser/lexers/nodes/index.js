5954eb5b543ed0aaf637fb8d0896e0396053f38d
Mark Howe <mvahowe@gmail.com>
2021-09-09 14:46:27 +0200

    Tests for trees

--- src/parser/lexers/nodes/index.js
+++ src/parser/lexers/nodes/index.js
@@ -1,4 +1,46 @@
+const { labelForScope } = require('proskomma-utils');
 const { Sequence } = require('../../model/sequence');
+const { tokenizeString } = require('../../../parser/lib/tokenize');
+
+let nextNodeId = 0;
+
+const numberNodes = (node, parentId) => {
+  const ret = {
+    ...node,
+    id: nextNodeId,
+    parentId: typeof parentId === 'number' ? parentId : 'none',
+  };
+  nextNodeId++;
+
+  if (node.children) {
+    ret.children = node.children.map(cn => numberNodes(cn, ret.id));
+  }
+  return ret;
+};
+
+const flattenNodes = node => {
+  const ret = [{}];
+  ret[0].id = node.id;
+  ret[0].parentId = node.parentId;
+
+  if (node.primary) {
+    ret[0].primary = node.primary;
+  }
+
+  if (node.secondary) {
+    ret[0].secondary = node.secondary;
+  }
+
+  if (node.children) {
+    ret[0].children = [];
+
+    for (const cn of node.children) {
+      ret[0].children.push(cn.id);
+      flattenNodes(cn).forEach(n => ret.push(n));
+    }
+  }
+  return ret;
+};
 
 const parseNodes = (str, parser, bookCode) => {
   parser.headers.id = bookCode;
@@ -6,13 +48,99 @@ const parseNodes = (str, parser, bookCode) => {
   const treeSequence = new Sequence('tree');
   const treeContentSequence = new Sequence('treeContent');
 
-  parser.sequences.tree.push(treeSequence);
-  parser.sequences.treeContent.push(treeContentSequence);
   treeSequence.addBlockGraft({
     type: 'graft',
     subType: 'treeContent',
     payload: treeContentSequence.id,
   });
+
+  for (const node of flattenNodes(numberNodes(JSON.parse(str)))) {
+    treeSequence.newBlock(labelForScope('tTreeNode', [`${node.id}`]));
+    const scopePayload = labelForScope('tTreeParent', [`${node.parentId}`]);
+    treeSequence.lastBlock().items.push({
+      type: 'scope',
+      subType: 'start',
+      payload: scopePayload,
+    });
+    treeSequence.lastBlock().items.push({
+      type: 'scope',
+      subType: 'end',
+      payload: scopePayload,
+    });
+
+    if (node.primary || node.secondary) {
+      treeContentSequence.newBlock(labelForScope('tTreeNode', [`${node.id}`]));
+
+      if (node.primary) {
+        const treeContentStart = treeContentSequence.lastBlock().items.length;
+        const tokenized = tokenizeString(node.primary);
+        const scopePayload = labelForScope('tTreeContent', ['-', node.id, `${treeContentStart}`, `${tokenized.length}`]);
+
+        treeSequence.lastBlock().items.push({
+          type: 'scope',
+          subType: 'start',
+          payload: scopePayload,
+        });
+        treeSequence.lastBlock().items.push({
+          type: 'scope',
+          subType: 'end',
+          payload: scopePayload,
+        });
+
+        for (const [payload, subType] of tokenized) {
+          treeContentSequence.lastBlock().items.push({
+            type: 'token',
+            subType,
+            payload,
+          });
+        }
+      }
+      if (node.secondary) {
+        for (const [name, content] of Object.entries(node.secondary)) {
+          const treeContentStart = treeContentSequence.lastBlock().items.length;
+          const tokenized = tokenizeString(content);
+          const scopePayload = labelForScope('tTreeContent', [name, node.id, `${treeContentStart}`, `${tokenized.length}`]);
+
+          treeSequence.lastBlock().items.push({
+            type: 'scope',
+            subType: 'start',
+            payload: scopePayload,
+          });
+          treeSequence.lastBlock().items.push({
+            type: 'scope',
+            subType: 'end',
+            payload: scopePayload,
+          });
+
+          for (const [payload, subType] of tokenized) {
+            treeContentSequence.lastBlock().items.push({
+              type: 'token',
+              subType,
+              payload,
+            });
+          }
+        }
+      }
+      if (node.children) {
+        for (const [childN, childNodeN] of node.children.entries()) {
+          const scopePayload = labelForScope('tTreeChild', [childN, childNodeN]);
+          treeSequence.lastBlock().items.push({
+            type: 'scope',
+            subType: 'start',
+            payload: scopePayload,
+          });
+          treeSequence.lastBlock().items.push({
+            type: 'scope',
+            subType: 'end',
+            payload: scopePayload,
+          });
+        }
+      }
+    }
+  }
+
+  parser.sequences.tree.push(treeSequence);
+  parser.sequences.treeContent.push(treeContentSequence);
   parser.sequences.main.addBlockGraft({
     type: 'graft',
     subType: 'tree',
--- test/code/nodes.js
+++ test/code/nodes.js
@@ -10,10 +10,58 @@ const importNodes = pk => {
   }, 'nodes',
   JSON.stringify(
     {
-      rows: [
-        ['ab.c', 'c ba'],
-        ['de.f', 'f ed'],
-        ['gh.i', 'i hg'],
+      primary: 'me',
+      secondary: {
+        name: 'Fred Smith',
+        shoeSize: '78',
+      },
+      children: [
+        {
+          primary: 'mom',
+          secondary: {
+            name: 'Sally Smith née Brown',
+            shoeSize: '3',
+          },
+          children: [
+            {
+              primary: 'grandma',
+              secondary: {
+                name: 'Emma Smith née Jones',
+                shoeSize: '2',
+              },
+            },
+            {
+              primary: 'grandpa',
+              secondary: {
+                name: 'Simon Smith',
+                shoeSize: '89',
+              },
+            },
+          ],
+        },
+        {
+          primary: 'pop',
+          secondary: {
+            name: 'Bob Smith',
+            shoeSize: '91',
+          },
+          children: [
+            {
+              primary: 'granny',
+              secondary: {
+                name: 'Deborah Smith née Black',
+                shoeSize: '5',
+              },
+            },
+            {
+              primary: 'grandpops',
+              secondary: {
+                name: 'Michael Smith',
+                shoeSize: '79',
+              },
+            },
+          ],
+        },
       ],
     },
   ),
@@ -27,13 +75,34 @@ test(
   `Import (${testGroup})`,
   async function (t) {
     try {
-      t.plan(1);
+      t.plan(12);
       const pk = new Proskomma();
       importNodes(pk);
       let query = '{documents { mainSequence { id } sequences { id type tags blocks { bs { payload} bg { payload } is { payload } tokens { type subType payload } } } } }';
       let result = await pk.gqlQuery(query);
-      console.log(JSON.stringify(result, null, 2));
       t.equal(result.errors, undefined);
+      const doc = result.data.documents[0];
+      const mainSequence = doc.sequences.filter(s => s.id === doc.mainSequence.id)[0];
+      const treeGraft = mainSequence.blocks[0].bg[0].payload;
+      const treeSequence = doc.sequences.filter(s => s.id === treeGraft)[0];
+      const treeContentGraft = treeSequence.blocks[0].bg[0].payload;
+      const treeContentSequence = doc.sequences.filter(s => s.id === treeContentGraft)[0];
+      t.equal(treeSequence.type, 'tree');
+      t.equal(treeSequence.blocks.length, 7);
+      t.equal(treeSequence.blocks[0].bs.payload, 'tTreeNode/0');
+      const nodeChildren = treeSequence.blocks[0].is.filter(s => s.payload.startsWith('tTreeChild')).map(s => s.payload);
+      t.equal(nodeChildren.length, 2);
+      const nodeParent = treeSequence.blocks[0].is.filter(s => s.payload.startsWith('tTreeParent')).map(s => s.payload);
+      t.equal(nodeParent.length, 1);
+      t.equal(nodeParent[0], 'tTreeParent/none');
+      const nodeName = treeSequence.blocks[0].is.filter(s => s.payload.startsWith('tTreeContent/name')).map(s => s.payload);
+      t.equal(nodeName.length, 1);
+      const [ssT, ssName, ssBlock, ssStart, ssLength] = nodeName[0].split('/');
+      t.equal(treeContentSequence.type, 'treeContent');
+      t.equal(treeContentSequence.blocks.length, 7);
+      const nameContent = treeContentSequence.blocks[parseInt(ssBlock)].tokens.slice(parseInt(ssStart), parseInt(ssStart) + parseInt(ssLength));
+      t.equal(nameContent.length, 3);
+      t.equal(nameContent.map(t => t.payload).join(''), 'Fred Smith');
     } catch (err) {
       console.log(err);
     }
