6fdfd3dba4f761381ee7ab03ddc30cfd54a35ed0
Mark Howe <mvahowe@gmail.com>
2021-09-14 20:03:28 +0200

    node

--- src/graph/lib/tribos.js
+++ src/graph/lib/tribos.js
@@ -1,18 +1,19 @@
 import xre from 'xregexp';
 
+// Nodes with one of the listed ids
 const doAbsoluteIdStep = (docSet, allNodes, result, queryStep, matches) => {
-  // Nodes with one of the listed ids
   const values = matches[1].split(',').map(v => v.trim());
   return { data: allNodes.filter(n => values.includes(docSet.unsuccinctifyScopes(n.bs)[0][2].split('/')[1])) };
 };
 
-const doAbsoluteRootStep = (docSet, allNodes) => {
-  // The root Node
-  return { data: [allNodes[0]] };
-};
+// The root Node
+const doAbsoluteRootStep = (docSet, allNodes) => ({ data: [allNodes[0]] });
 
+// All the nodes
+const doAbsoluteNodesStep = (docSet, allNodes) => ({ data: allNodes });
+
+// Children of the nodes
 const doChildrenStep = (docSet, allNodes, result) => {
-  // Children of the nodes
   const childNodeIds = new Set([]);
 
   for (const parentNode of result.data) {
@@ -27,8 +28,8 @@ const doChildrenStep = (docSet, allNodes, result) => {
   return { data: allNodes.filter(n => childNodeIds.has(docSet.unsuccinctifyScopes(n.bs)[0][2].split('/')[1])) };
 };
 
+// The parent of each node
 const doParentStep = (docSet, allNodes, result) => {
-  // The parent of each node
   const parentNodeIds = new Set([]);
 
   for (const childNode of result.data) {
@@ -40,10 +41,10 @@ const doParentStep = (docSet, allNodes, result) => {
   return { data: allNodes.filter(n => parentNodeIds.has(docSet.unsuccinctifyScopes(n.bs)[0][2].split('/')[1])) };
 };
 
+// The children of the parent of each node
+// - get parent of starting node
+// - get children of parent node
 const doSiblingsStep = (docSet, allNodes, result) => {
-  // The children of the parent of each node
-  // - get parent of starting node
-  // - get children of parent node
   const parentNodeIds = new Set([]);
 
   for (const childNode of result.data) {
@@ -68,6 +69,34 @@ const doSiblingsStep = (docSet, allNodes, result) => {
   return { data: allNodes.filter(n => childNodeIds.has(docSet.unsuccinctifyScopes(n.bs)[0][2].split('/')[1])) };
 };
 
+// The node details
+const doNodeStep = (docSet, allNodes, result) => {
+  const ret = [];
+
+  for (const node of result.data) {
+    const record = {};
+
+    record.id = docSet.unsuccinctifyScopes(node.bs)[0][2].split('/')[1];
+
+    record.parentId = docSet.unsuccinctifyScopes(node.is)
+      .filter(s => s[2].startsWith('tTreeParent'))
+      .map(s => s[2].split('/')[1])[0];
+
+    const content = {};
+
+    for (const [scopeLabels, items] of docSet.sequenceItemsByScopes([node], ['tTreeContent/'], false)) {
+      const key = scopeLabels.filter(s => s.startsWith('tTreeContent'))[0].split('/')[1];
+      content[key] = items.filter(i => i[0] === 'token').map(t => t[2]).join('');
+    }
+
+    if (Object.keys(content).length > 0) {
+      record.content = content;
+    }
+    ret.push(record);
+  }
+  return ret;
+};
+
 const stepActions = [
   [
     xre('^#\\{([^}]+)\\}$'),
@@ -77,6 +106,11 @@ const stepActions = [
   [
     xre('^root$'),
     doAbsoluteRootStep,
+    new Set(['absolute']),
+  ],
+  [
+    xre('^nodes$'),
+    doAbsoluteNodesStep,
     new Set(['absolute']),
   ],
   [
@@ -93,6 +127,11 @@ const stepActions = [
     xre('^siblings$'),
     doSiblingsStep,
     new Set([]),
+  ],
+  [
+    xre('^node$'),
+    doNodeStep,
+    new Set(['last']),
   ],
 ];
 
