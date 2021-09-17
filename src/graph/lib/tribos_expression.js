1a171d8c3b1e3cdf3273e64bcefd09f9755d097c
Mark Howe <mvahowe@gmail.com>
2021-09-17 15:57:55 +0200

    Nested predicate operators

--- src/graph/lib/tribos_expression.js
+++ src/graph/lib/tribos_expression.js
@@ -3,10 +3,12 @@ import xre from 'xregexp';
 const aggregateFunctions = {
   ids: () => console.log('ids'),
   equals: (a, b) => a === b,
+  and: (...args) => args.filter(a => !a).length === 0,
+  or: (...args) => args.filter(a => a).length > 0,
+  not: a => !a,
 };
 
 const parseFunctions = {
-  csArgs: str => str.split(',').map(s => s.trim()),
   quotedString: str => str.substring(1, str.length - 1),
   idRef: () => console.log('idRef'),
   parentIdRef: () => console.log('parentIdRef'),
@@ -15,15 +17,47 @@ const parseFunctions = {
   false: () => false,
 };
 
+const splitArgs = str => {
+  const ret = [[]];
+  let pos = 0;
+  let nParen = 0;
+
+  while (pos < str.length) {
+    switch(str[pos]) {
+    case '(':
+      ret[ret.length - 1].push(str[pos]);
+      nParen++;
+      break;
+    case ')':
+      ret[ret.length - 1].push(str[pos]);
+      nParen--;
+      break;
+    case ',':
+      if (nParen === 0) {
+        ret.push([]);
+        while (str[pos + 1] === ' ') {
+          pos++;
+        }
+      } else {
+        ret[ret.length - 1].push(str[pos]);
+      }
+      break;
+    default:
+      ret[ret.length - 1].push(str[pos]);
+    }
+    pos++;
+  }
+  return ret.map(e => e.join(''));
+};
+
 const expressions = [
   {
     id: 'booleanExpression',
-    oneOf: ['booleanPrimitive', 'equals'],
+    oneOf: ['booleanPrimitive', 'equals', 'and', 'or', 'not'],
   },
   {
     id: 'ids',
     regex: xre('^#\\{([^}]+)\\}$'),
-    parseFunctions: [null, 'csArgs'],
     argStructure: [['id', [1, null]]],
   },
   {
@@ -33,9 +67,23 @@ const expressions = [
   },
   {
     id: 'equals',
-    regex: xre('^==\\(([^}]+)\\)$'),
-    parseFunctions: [null, 'csArgs'],
+    regex: xre('^==\\((.+)\\)$'),
     argStructure: [['stringPrimitive', [2, 2]]],
+  },
+  {
+    id: 'and',
+    regex: xre('^and\\((.+)\\)$'),
+    argStructure: [['stringPrimitive', [2, null]]],
+  },
+  {
+    id: 'or',
+    regex: xre('^or\\((.+)\\)$'),
+    argStructure: [['stringPrimitive', [2, null]]],
+  },
+  {
+    id: 'not',
+    regex: xre('^not\\((.+)\\)$'),
+    argStructure: [['stringPrimitive', [1, 1]]],
   },
   {
     id: 'stringPrimitive',
@@ -49,43 +97,93 @@ const expressions = [
   },
 ];
 
-const doPredicate1 = (result, predicateString) => {
-  for (const expression of expressions) {
-    if (!expression.regex) {
+const parseRegexExpression = (result, predicateString, expressionId, matches) => {
+  // console.log(`parseRegexExpression ${predicateString} ${expressionId} ${matches}`);
+  const expressionRecord = expressions.filter(e => e.id === expressionId)[0];
+
+  if (!expressionRecord) {
+    throw new Error(`Unknown expression ${expressionId} for predicate ${predicateString}`);
+  }
+
+  let found = false;
+
+  if (expressionRecord.parseFunctions) {
+    for (const [n, parseFunction] of expressionRecord.parseFunctions.entries()) {
+      if (!parseFunction || !matches[n]) {
+        continue;
+      }
+      found = true;
+      return { data: parseFunctions[parseFunction](matches[n]) };
+    }
+
+    if (!found) {
+      return { errors: `Could not parse predicate ${predicateString}` };
+    }
+  } else {
+    const argRecords = splitArgs(matches[1]).map(a => parseExpressions(result, a));
+
+    if (argRecords.filter(ar => ar.errors).length === 0) {
+      const aggregated = aggregateFunctions[expressionId](...argRecords.map(ar => ar.data));
+      console.log('aggregated', expressionId, argRecords, aggregated);
+      return { data:  aggregated };
+    }
+    return { errors: `Could not parse arguments to ${expressionId}` };
+  }
+};
+
+const parseExpressions = (result, predicateString) => {
+  // console.log(`parseExpressions ${predicateString}`);
+
+  for (const expressionRecord of expressions) {
+    if (!expressionRecord.regex) {
       continue;
     }
 
-    const matches = xre.exec(predicateString, expression.regex);
+    const matches = xre.exec(predicateString, expressionRecord.regex);
 
     if (matches) {
-      let found = false;
-
-      for (const [n, parseFunction] of expression.parseFunctions.entries()) {
-        if (!parseFunction || !matches[n]) {
-          continue;
-        }
-        found = true;
-        const parsed = parseFunctions[parseFunction](matches[n]);
-
-        if (expression.argStructure) {
-          console.log('aggregator!');
-          const args = doPredicate1(result, parsed.map(p => doPredicate1(result, p)));
-          console.log('args', args);
-          // aggregateFunctions[expression.id](...args);
-        } else {
-          console.log('primitive!');
-          console.log(parsed);
-        }
-      }
-
-      if (!found) {
-        console.log(`Could not parse predicate ${predicateString}`);
-      }
+      return parseRegexExpression(result, predicateString, expressionRecord.id, matches);
     }
   }
-  return result;
+  return { error: `No regex match for ${predicateString}` };
 };
 
-const doPredicate = (result, predicateString) => doPredicate1(result, predicateString);
+const parseExpression = (result, predicate, expressionId) => {
+  // console.log(`parseExpression ${predicate}, ${expressionId}`);
+  const expressionRecord = expressions.filter(e => e.id === expressionId)[0];
+
+  if (!expressionRecord) {
+    throw new Error(`Unknown expression ${expressionId} for predicate ${predicate}`);
+  }
+
+  if (expressionRecord.oneOf) {
+    const errors = [];
+
+    for (const option of expressionRecord.oneOf) {
+      const optionResult = parseExpression(result, predicate, option);
+
+      if (!optionResult.errors) {
+        return optionResult;
+      } else {
+        errors.push(`Could not parse ${predicate} as ${option}`);
+      }
+    }
+    return { errors: errors.join('; ') };
+  } else {
+    const matches = xre.exec(predicate, expressionRecord.regex);
+
+    if (matches) {
+      const reResult = parseRegexExpression(result, predicate, expressionId, matches);
+      return reResult;
+    } else {
+      return { errors: `Could not match ${predicate} to ${expressionId}` };
+    }
+  }
+};
+
+const doPredicate = (result, predicateString) => {
+  console.log('doPredicate', parseExpression(result, predicateString, 'booleanExpression'));
+  return result;
+};
 
 module.exports = { doPredicate };
