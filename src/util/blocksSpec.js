74f3b6d49557df79448de87e1df8015e4a24d5e9
Mark Howe <mvahowe@gmail.com>
2021-11-15 12:11:29 +0100

    Handle symbol modifiers and tabs

--- src/util/blocksSpec.js
+++ src/util/blocksSpec.js
@@ -154,6 +154,7 @@ const escapePayload =
       str.replace(/\\/g, '\\\\')
         .replace(/"/g, '\\"')
         .replace(/\n/g, '\\n')
+        .replace(/\t/g, '\\t')
         .replace(/\r/g, '\\r');
 
 const object2Query = obs =>
