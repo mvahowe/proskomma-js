f05421ef402cb4146ebc3dea35e53f8a58244b0f
Mark Howe <mvahowe@gmail.com>
2022-10-14 13:39:15 +0200

    Add do_sofria script

---
+++ scripts/do_sofria.js
@@ -0,0 +1,46 @@
+const fse = require('fs-extra');
+const proskommaUtils = require('proskomma-utils');
+
+const { Proskomma } = require('../dist/index.js');
+
+if (process.argv.length < 3 || process.argv.length > 4) {
+  console.log('USAGE: node do_sofria.js <USFM/USX Path> [chN]');
+  process.exit(1);
+}
+
+const contentPath = process.argv[2];
+let content;
+
+try {
+  content = fse.readFileSync(contentPath);
+} catch (err) {
+  console.log(`ERROR: Could not read from USFM/USX file '${contentPath}'`);
+  process.exit(1);
+}
+
+const contentType = contentPath.split('.').pop();
+let query = `{ documents { sofria${process.argv[3] ? "(chapter: " + process.argv[3] + ")": ""} } }`;
+
+const pk = new Proskomma();
+//try {
+let selectors = {
+  lang: 'lan',
+  abbr: 'myabbr',
+};
+
+pk.importDocument(
+  selectors,
+  contentType,
+  content,
+);
+/*
+} catch (err) {
+console.log(`ERROR: Could not import document: '${err}'\n`);
+console.trace();
+process.exit(1);
+}
+
+ */
+pk.gqlQuery(query)
+  .then(output => console.log(JSON.stringify(JSON.parse(output.data.documents[0].sofria), null, 2)))
+  .catch(err => console.log(`ERROR: Could not run query: '${err}'`));
